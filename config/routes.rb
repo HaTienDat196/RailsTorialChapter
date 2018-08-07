c087fd854e112657edfa80cde4fece042ca84c51
TienDat <hatiendat0803@gmail.com>
2018-08-07 10:28:14 +0700

    finish layout anh routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/pages'
-  get 'static_pages/about'
+  get 'users/new'
   root 'static_pages#home'
+  get  '/pages',    to: 'static_pages#pages', as: 'pages'
+  get  '/about',   to: 'static_pages#about'
+  get  '/signup',  to: 'users#new'
 end
