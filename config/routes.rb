e8d4460a3c81d0386d8b6fe620049978e2d3a7c2
TienDat <hatiendat0803@gmail.com>
2018-08-09 08:57:29 +0700

    basic_login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  root 'static_pages#home'
-  get  '/pages',    to: 'static_pages#pages', as: 'pages'
-  get  '/about',   to: 'static_pages#about'
-  get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
+  root   'static_pages#home'
+  get    '/pages',    to: 'static_pages#pages'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
--- test/integration/users_singup_test.rb
+++ test/integration/users_singup_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -11,5 +10,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
